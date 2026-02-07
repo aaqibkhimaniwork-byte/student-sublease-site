6bbb4e6035d70ab81822b5c67d177a04ae9f28f3
Gary Chen <gzc11036@uga.edu>
2026-02-06 23:34:26 -0500

    made the header for splash page and created css files for splash page, home, createlisting, signup, and login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link, useLocation } from "react-router-dom";
 
+import SplashPage from "./pages/SplashPage";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
@@ -8,21 +9,30 @@ import CreateListing from "./pages/CreateListing";
 export default function App() {
   return (
     <BrowserRouter>
-      {/* Simple Navbar */}
-      <nav style={{ padding: "15px", background: "#eee" }}>
-        <Link to="/" style={{ marginRight: "15px" }}>Signup</Link>
-        <Link to="/login" style={{ marginRight: "15px" }}>Login</Link>
-        <Link to="/home" style={{ marginRight: "15px" }}>Listings</Link>
-        <Link to="/create">Post Listing</Link>
-      </nav>
+      <TopBarIfNotSplash />
 
       {/* Routes */}
       <Routes>
-        <Route path="/" element={<Signup />} />
+        <Route path="/" element={<SplashPage />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/create" element={<CreateListing />} />
       </Routes>
     </BrowserRouter>
+  );
+}
+
+function TopBarIfNotSplash() {
+  const location = useLocation();
+  if (location.pathname === "/") return null;
+
+  return (
+    <nav style={{ padding: "15px", background: "#eee" }}>
+      <Link to="/signup" style={{ marginRight: "15px" }}>Signup</Link>
+      <Link to="/login" style={{ marginRight: "15px" }}>Login</Link>
+      <Link to="/home" style={{ marginRight: "15px" }}>Listings</Link>
+      <Link to="/create">Post Listing</Link>
+    </nav>
   );
 }
