7369652781abc9a77e281aa7154ff1126a787dd4
Gary Chen <gzc11036@uga.edu>
2026-02-07 22:32:14 -0500

    Update Listings.js

--- src/pages/Listings.js
+++ src/pages/Listings.js
@@ -330,7 +330,7 @@ export default function Listings() {
                       style={creatorBadgeStyle} 
                       onClick={(e) => {
                         e.stopPropagation();
-                        navigate(`/MyProfile/${listing.user_id}`);
+                        navigate(`/profile/${listing.user_id}`);
                       }}
                     >
                       <img 
